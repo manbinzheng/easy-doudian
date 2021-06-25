ab4f6f488bd73b958d5be8e65ec7c2ddeda6e2b8
manbinzheng <894775625@qq.com>
2021-06-25 11:23:11 +0800

    change

--- src/EasyDouDian.php
+++ src/EasyDouDian.php
@@ -6,18 +6,6 @@ namespace ManbinZheng\EasyDouDian;
 
 class EasyDouDian extends Base
 {
-    public $shop;
-    public $product;
-    public $order;
-
-    public function __construct($app_key, $app_secret)
-    {
-        if (!$app_key || !$app_secret) throw new \Exception('请先配置app_key与app_secret！');
-        $this->shop = new Shop($app_key, $app_secret);
-        $this->product = new Product($app_key, $app_secret);
-        $this->order = new Order($app_key, $app_secret);
-    }
-
 
     /**
      * @param string $method 调用的api接口名称
